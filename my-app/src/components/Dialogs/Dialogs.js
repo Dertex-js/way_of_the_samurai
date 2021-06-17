db1ddc21feb6605b066135616ed478a3d88c9c03
Dertex-js <ryoamir60@gmail.com>
2021-06-18 01:04:00 +0500

    Initial commit

---
+++ my-app/src/components/Dialogs/Dialogs.js
@@ -0,0 +1,36 @@
+import React from "react";
+import s from "./Dialogs.module.css"
+
+const Dialogs = (props) => {
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + " " + s.active}>
+                    11
+                </div>
+                <div className={s.dialog}>
+                    2
+                </div>
+                <div className={s.dialog}>
+                    3
+                </div>
+                <div className={s.dialog}>
+                    4
+                </div>
+                <div className={s.dialog}>
+                    5
+                </div>
+                <div className={s.dialog}>
+                    6
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are u?</div>
+                <div className={s.message}>Nigga</div>
+            </div>
+        </div>
+    );
+}
+
+export default Dialogs;
