6ddcf5abf422c9eea640fb6975539a3bdac628be
Dertex-js <ryoamir60@gmail.com>
2021-09-06 00:30:30 +0500

    FLUX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -14,12 +14,15 @@ const App = (props) => {
                 <Nav state={props.state.friendsNav.friends}/>
                 <div className="app-wrapper-content">
                     <Route path="/dialogs"
-                           render={ () => <Dialogs
-                               state={props.state.dialogsPage} /> }/>
+                           render={() => <Dialogs
+                               state={props.state.dialogsPage}/>}/>
                     <Route path="/profile"
-                           render={ () => <Profile
-                               state={props.state.profilePage}
-                               addPost={props.addPost}/> }/>
+                           render={() => <Profile
+                               profilePage={props.state.profilePage}
+                               addPost={props.addPost}
+                               updateNewPostText={props.updateNewPostText}
+                           />}
+                    />
                 </div>
             </div>
         </BrowserRouter>
--- my-app/src/redux/state.js
+++ my-app/src/redux/state.js
@@ -21,6 +21,7 @@ let state = {
             {id: 1, message: "Hi, how are u?", likesCount: 12},
             {id: 2, message: "It's my first post", likesCount: 23}
         ],
+        newPostText: "asdasda",
     },
     friendsNav: {
         friends: {
@@ -40,13 +41,21 @@ let state = {
     }
 }
 
-export let addPost = (postMessage) => {
+window.state = state
+
+export let addPost = () => {
     let newPost = {
         id: 5,
-        message: postMessage,
+        message: state.profilePage.newPostText,
         likesCount: 0
     }
     state.profilePage.posts.push(newPost)
+    state.profilePage.newPostText = ""
+    rerenderEntireTree(state)
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText
     rerenderEntireTree(state)
 }
 
